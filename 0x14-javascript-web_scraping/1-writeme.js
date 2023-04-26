7cc4faeb68c520816995aa3b695db1a14cca1358
ZEDD2468 <victoradebayo360@gmail.com>
2023-04-26 20:15:58 +0000

    JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// This code is a Node.js script that writes content to a file specified by a command line argument. Here's how it works: The first line (#!/usr/bin/node) is a shebang that tells the operating system to run the script using the Node.js interpreter.
+//The fs module is imported using const fs = require('fs');. This module provides an API for working with the file system in Node.js.//
+//fs.writeFile() is called to write the content specified by the third command line argument (process.argv[3]) to the file specified by the second command line argument (process.argv[2]).//
+//The callback function passed to fs.writeFile() takes one argument, error, which is called if there is an error while writing to the file.
+//If an error occurs during writing, the error object is logged to the console using console.log(error).//
+//So essentially, this script writes content to a file and logs an error message to the console if there is an error while writing to the file. Note that if the file specified by the second command line argument does not exist, it will be created. If it already exists, its contents will be overwritten.//
+
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
